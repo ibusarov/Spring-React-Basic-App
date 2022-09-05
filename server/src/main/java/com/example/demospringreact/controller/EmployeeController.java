b34ac28f6316698bc0c88d76d3d6e9b789cca37d
Igor Busarov <igobusarov@yahoo.com>
2022-09-06 01:56:06 +0300

    Update and delete backend without auth

--- server/src/main/java/com/example/demospringreact/controller/EmployeeController.java
+++ server/src/main/java/com/example/demospringreact/controller/EmployeeController.java
@@ -4,11 +4,13 @@ package com.example.demospringreact.controller;
 import com.example.demospringreact.exception.ResourceNotFoundException;
 import com.example.demospringreact.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.demospringreact.repository.EmployeeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -33,6 +35,7 @@ public class EmployeeController {
     }
 
     //create employee
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
 
@@ -50,8 +53,8 @@ public class EmployeeController {
     }
 
     //update employee rest
-    @PutMapping(value = "/emloyees/{id}",
-            consumes = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/emloyees/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee detailedEmployee){
 
         Employee employee = employeeRepository.findById(id)
@@ -66,4 +69,19 @@ public class EmployeeController {
         return ResponseEntity.ok(updatedEmployee);
 
     }
+
+    //Delete method
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/emloyees/{id}")
+    public void deleteEmployee(@PathVariable Long id){
+
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if(employee.isEmpty()){
+            throw new ResourceNotFoundException("Employee with id: " + id + " not found");
+        }else {
+            employeeRepository.deleteById(id);
+        }
+    }
+
 }
